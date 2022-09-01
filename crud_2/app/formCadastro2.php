10035fb83580f7dffc2e06dfee4302213935cff4
cleidson santana <cleidsonsantana@gmail.com>
2022-09-01 10:04:28 -0300

    estilizando a pagina formCadastro

--- crud_2/app/formCadastro2.php
+++ crud_2/app/formCadastro2.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="styles_2.css">
     <title>Cadastro</title>
 </head>
 
@@ -13,14 +13,14 @@
         <form action="cadastrar2.php" method="POST">
             <h3>CRIANÇAS CADASTRO</h3>
             <div>
-                <label for="nome">Nome da Criança</label><br>
-                <input type="text" name="nome" id="nome" required><br>
+                <label for="nome">Nome da Criança</label>
+                <input type="text" name="nome" id="nome" required>
 
-                <label for="telefone">Nome do Pai</label><br>
-                <input type="text" name="pai" id="pai" required><br>
+                <label for="telefone">Nome do Pai</label>
+                <input type="text" name="pai" id="pai" required>
 
-                <label for="email">Nome da Mãe</label><br>
-                <input type="text" name="mae" id="mae" required><br>
+                <label for="email">Nome da Mãe</label>
+                <input type="text" name="mae" id="mae" required>
             </div>
             <input type="submit" value="CADASTRAR" name=""  id="">
         </form>
