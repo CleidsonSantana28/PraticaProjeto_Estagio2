a46d5b2425751bbe82483c2093e65588b3ad7ce1
cleidson santana <cleidsonsantana@gmail.com>
2022-09-05 10:57:52 -0300

    conexão do banco de dados 'cadastro' e com a class Pessoa()

--- CRUD_PESSOA/app/index.php
+++ CRUD_PESSOA/app/index.php
@@ -1,17 +1,22 @@
+<?php
+require_once 'classPessoa.php';
+$p = new Pessoa("cadastrosimples", "127.0.0.1", "cleidson", "C@santos123");
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilo.css">
     <title>Principal</title>
 </head>
+
 <body>
     <section id="esquerda">
         <form action="">
             <h2>CADASTRAR PESSOA</h2>
-            
+
             <label for="nome">Nome</label>
             <input type="text" name="nome" id="nome">
 
@@ -25,23 +30,37 @@
         </form>
 
     </section>
-    
+
     <section id="direita">
         <table>
             <tr id="titulo">
                 <td>Nome</td>
-                <td>Telefone</td>
-                <td colspan="2">Email</td>
+                <td>Email</td>
+                <td colspan="2">Telefone</td>
             </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td><a href="">Editar</a><a href="">Excluir</a></td>
-            </tr>
+            <?php
+                $dados = $p->buscarDados();
+                if (count($dados) > 0) {
+                for ($i = 0; $i < count($dados); $i++) {
+                    echo "<tr>";
+                    foreach ($dados[$i] as $key => $value) {
+                        if ($key != "idContato") {
+                            echo "<td>" . $value . "</td>";
+                        }
+                    }
+            ?>
+                    <td><a href="">Editar</a><a href="">Excluir</a></td>
+                <?php
+                    echo "</tr>";
+                }
+                ?>
+            <?php
+            }
+            ?>
         </table>
 
     </section>
-    
+
 </body>
+
 </html>
